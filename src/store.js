5316060faab9a3048a07800fa04f88e6981ef4d6
hee-c <ways2kim@gmail.com>
2021-04-16 01:21:25 +0900

    Add initial files

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import reducer from './reducers'
+
+const middleware = [ReduxThunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+export default createStore(reducer, applyMiddleware(...middleware));
