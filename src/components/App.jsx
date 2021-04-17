aabc0d81e24902fb2d005f9f135d8a26e4889fda
hee-c <ways2kim@gmail.com>
2021-04-17 15:59:52 +0900

    Add Footer component, Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './Header';
-import HomeMain from './HomeMain';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <Header />
-      <HomeMain />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </Router>
   );
 }
--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import mainImg from '../assets/mainImg.png';
+import Footer from './Footer';
 
 const HomeWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 56px;
+  margin-top: 80px;
   height: calc(100vh - 80px);
+  width: 100%;
 `;
 
 const HomeSection = styled.div`
@@ -25,8 +27,14 @@ const ContentWrapper = styled.div`
 const Content = styled.div`
   display: flex;
   flex-direction: column;
-  height: 50%;
+  height: ${props => props.height || '50%'};
   margin: auto;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  width: 50%;
+  height: 50%;
 `;
 
 export default function HomeMain() {
@@ -41,7 +49,7 @@ export default function HomeMain() {
         <ContentWrapper>
           <Content>
             <h2>
-              헬스케어 기업을 위한<br/>
+              헬스케어 기업을 위한<br />
               최적의 광고 플랫폼
             </h2>
             <span>니즈가 명확한 고객을 확보하세요</span>
@@ -51,35 +59,42 @@ export default function HomeMain() {
           </Content>
         </ContentWrapper>
         <ContentWrapper>
-          <img src={mainImg} alt="mainImg"/>
+          <ImgContainer>
+            <img src={mainImg} alt="mainImg" />
+          </ImgContainer>
         </ContentWrapper>
       </HomeSection>
       <HomeSection>
         <ContentWrapper>
           <Content>
             <h2>
-              내 비지니스에<br/>
-              적합한 예산을<br/>
+              내 비지니스에<br />
+              적합한 예산을<br />
               설정하세요
             </h2>
             <span>
-              Google Ads는 광고 예산의 규모에 상관없이 이용할 수 있습니다.<br/>
-              월예산 한도를 설정하면 그 이상 지출되는 경우가 없습니다. <br/>
+              Google Ads는 광고 예산의 규모에 상관없이 이용할 수 있습니다.<br />
+              월예산 한도를 설정하면 그 이상 지출되는 경우가 없습니다. <br />
               또한 언제든지 지출을 일시중지하거나 조정할 수 있습니다.
             </span>
           </Content>
         </ContentWrapper>
         <ContentWrapper>
-          <img src={mainImg} alt="mainImg"/>
+          <ImgContainer>
+            <img src={mainImg} alt="mainImg" />
+          </ImgContainer>
         </ContentWrapper>
       </HomeSection>
       <HomeSection>
-        <Content>
+        <Content height="100%">
           <h2>서비스 이름과 함께 딱 맞는 고객에게 다가가세요.</h2>
-          <img src={mainImg} alt="mainImg"/>
+          <ImgContainer>
+            <img src={mainImg} alt="mainImg" />
+          </ImgContainer>
           <span>서비스 Ads를 사용하면 예산 내에서 관련성 높은 고객에게 더 많이 도달할 수 있습니다.</span>
         </Content>
       </HomeSection>
+      <Footer />
     </HomeWrapper>
   );
 }
