2d9474750b282321c1af1f2784f593365d19f3b8
hyeongju <kokiguitars@gmail.com>
2021-04-17 12:28:08 +0900

    Add action constants and initial state for registerReducer

---
+++ src/reducers/registerReducer.js
@@ -0,0 +1,8 @@
+const REGISTER_REQUEST = 'REGISTER_REQUEST';
+const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'REGISTER_FAILURE';
+
+const initialState = {
+  isRegistering: false,
+  isError: false,
+};
