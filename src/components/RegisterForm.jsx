ab73272fd32279f25162cdd1bcfbcda170c59567
hyeongju <kokiguitars@gmail.com>
2021-04-17 19:06:46 +0900

    Add styled components in RegisterForm component

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,65 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Form = styled.form`
+  margin:auto 0;
+`;
+
+const Label = styled.div`
+  margin: 5px;
+`;
+
+const Input = styled.input`
+  display: block;
+
+`;
 
 export default function RegisterForm(props) {
   const { onRegisterInputChange, onRegisterFormSubmit, } = props;
 
   return (
-    <form name="form">
-      <label>Email</label>
-      <input
-        type="email"
-        name="email"
-        onChange={onRegisterInputChange}
-        required
-      />
-      <label>Name</label>
-      <input
-        type="text"
-        name="name"
-        onChange={onRegisterInputChange}
-        required
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        name="password"
-        minLength="8"
-        onChange={onRegisterInputChange}
-        required
-      />
-      <label>Confirm Password</label>
-      <input
-        type="password"
-        name="confirmPassword"
-        minLength="8"
-        onChange={onRegisterInputChange}
-        required
-      />
-      <label>Company Name</label>
-      <input
-        type="text"
-        name="companyName"
-        onChange={onRegisterInputChange}
-      />
-      <label>Company Email</label>
-      <input
-        type="text"
-        name="companyEmail"
-        onChange={onRegisterInputChange}
-      />
-      <label>Company Registration Number</label>
-      <input
-        type="text"
-        name="companyRegistrationNumber"
-        onChange={onRegisterInputChange}
-      />
-      <input
-        type="submit"
-        value="Register"
-        onSubmit={onRegisterFormSubmit}
-      />
-    </form>
+    <FormWrapper>
+      <Form name="form">
+        <Label>Email</Label>
+        <Input
+          type="email"
+          name="email"
+          onChange={onRegisterInputChange}
+          required
+        />
+        <Label>Name</Label>
+        <Input
+          type="text"
+          name="name"
+          onChange={onRegisterInputChange}
+          required
+        />
+        <Label>Password</Label>
+        <Input
+          type="password"
+          name="password"
+          minLength="8"
+          onChange={onRegisterInputChange}
+          required
+        />
+        <Label>Confirm Password</Label>
+        <Input
+          type="password"
+          name="confirmPassword"
+          minLength="8"
+          onChange={onRegisterInputChange}
+          required
+        />
+        <Label>Company Name</Label>
+        <Input
+          type="text"
+          name="companyName"
+          onChange={onRegisterInputChange}
+        />
+        <Label>Company Email</Label>
+        <Input
+          type="text"
+          name="companyEmail"
+          onChange={onRegisterInputChange}
+        />
+        <Label>Company Registration Number</Label>
+        <Input
+          type="text"
+          name="companyRegistrationNumber"
+          onChange={onRegisterInputChange}
+        />
+        <Input
+          type="submit"
+          value="Register"
+          onSubmit={onRegisterFormSubmit}
+        />
+      </Form>
+    </FormWrapper>
   );
 }
 
