12aafa0ec2814cc01c7d1504737ab138cfda293c
hyeongju <kokiguitars@gmail.com>
2021-04-17 20:16:03 +0900

    Add password confirmation in RegisterForm component

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 
 const FormWrapper = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
+  height: 100vh;
 `;
 
 const Form = styled.form`
@@ -22,62 +25,83 @@ const Input = styled.input`
 
 export default function RegisterForm(props) {
   const { onRegisterInputChange, onRegisterFormSubmit, } = props;
+  const {
+    register,
+    formState: { errors, },
+    getValues,
+    handleSubmit,
+  } = useForm();
 
   return (
     <FormWrapper>
-      <Form name="form">
+      <Form
+        name='form'
+        onSubmit={handleSubmit(onRegisterFormSubmit)}
+      >
         <Label>Email</Label>
         <Input
-          type="email"
-          name="email"
+          type='email'
+          {...register('email', { required: true, maxLength: 10, })}
+          name='email'
           onChange={onRegisterInputChange}
           required
         />
         <Label>Name</Label>
         <Input
-          type="text"
-          name="name"
+          type='text'
+          name='name'
           onChange={onRegisterInputChange}
           required
         />
         <Label>Password</Label>
         <Input
-          type="password"
-          name="password"
-          minLength="8"
+          type='password'
+          {...register('password')}
+          name='password'
+          minLength='8'
           onChange={onRegisterInputChange}
           required
         />
         <Label>Confirm Password</Label>
         <Input
-          type="password"
-          name="confirmPassword"
-          minLength="8"
+          type='password'
+          {...register('passwordConfirmation', {
+            validate: {
+              matchesPreviousPassword: (value) => {
+                const { password, } = getValues();
+                return password === value || 'password should match';
+              },
+            },
+          })}
+          name='passwordConfirmation'
+          minLength='8'
           onChange={onRegisterInputChange}
           required
         />
+        {errors.passwordConfirmation && (
+          <p>{errors.passwordConfirmation.message}</p>
+        )}
         <Label>Company Name</Label>
         <Input
-          type="text"
-          name="companyName"
+          type='text'
+          name='companyName'
           onChange={onRegisterInputChange}
         />
         <Label>Company Email</Label>
         <Input
-          type="text"
-          name="companyEmail"
+          type='text'
+          name='companyEmail'
           onChange={onRegisterInputChange}
         />
         <Label>Company Registration Number</Label>
         <Input
-          type="text"
-          name="companyRegistrationNumber"
+          type='text'
+          name='companyRegistrationNumber'
           onChange={onRegisterInputChange}
         />
         <Input
-          type="submit"
-          value="Register"
-          onSubmit={onRegisterFormSubmit}
+          type='submit'
+          value='Register'
         />
       </Form>
     </FormWrapper>
