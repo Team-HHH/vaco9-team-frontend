ebd6db7aacff31d575e87aba022b6a745dde9c3f
hyeongju <kokiguitars@gmail.com>
2021-04-17 12:31:56 +0900

    Add register reducer

--- src/reducers/registerReducer.js
+++ src/reducers/registerReducer.js
@@ -2,7 +2,20 @@ const REGISTER_REQUEST = 'REGISTER_REQUEST';
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 const REGISTER_FAILURE = 'REGISTER_FAILURE';
 
-const initialState = {
+const registerInitialState = {
   isRegistering: false,
   isError: false,
 };
+
+export function registerReducer(state = registerInitialState, action) {
+  switch (action.type) {
+    case REGISTER_REQUEST:
+      return { ...state };
+    case REGISTER_SUCCESS:
+      return { ...state };
+    case REGISTER_FAILURE:
+      return { ...state };
+    default:
+      return state;
+  }
+}
