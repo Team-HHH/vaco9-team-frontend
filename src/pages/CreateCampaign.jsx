25155d6377a46e7bc009ee5d091947172c19ac21
hee-c <ways2kim@gmail.com>
2021-04-18 15:32:33 +0900

    Add payment, new campaign fetch function

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import CampaignForm from '../components/CampaignForm';
+import { fetchNewCampaign, fetchPaymentResult } from '../apis/payment';
 
 const Container = styled.div`
   display: flex;
@@ -11,25 +12,43 @@ export default function CreateCampaign() {
   IMP.init(process.env.REACT_APP_IMPORT_ID);
 
   async function handleNewCampaignFormSubmit(data) {
+    console.log(data);
+    try {
+      const response = await fetchNewCampaign(data);
 
-    IMP.request_pay({
-      pg: 'html5_inicis',
-      pay_method: 'card',
-      merchant_uid: 'ORD20180131-0000012',
-      name: data.title,
-      amount: data.dailyBudget,
-      buyer_email: 'hcplays@gmail.com',
-      buyer_name: '김희찬',
-      buyer_tel: '010-8895-8278',
-    }, rsp => {
-      if (rsp.success) {
-        console.log('결제 성공');
-        console.log(rsp);
-      } else {
-        console.log('결제 실패');
-        console.log(rsp);
+      if (!response.ok) {
+        console.log('결제에 실패했습니다.');
+        return;
       }
-    });
+
+      const { merchantId } = response.body.data;
+
+      IMP.request_pay({
+        pg: 'html5_inicis',
+        pay_method: 'card',
+        merchant_uid: merchantId,
+        name: data.title,
+        amount: data.dailyBudget,
+        buyer_email: 'hcplays@gmail.com',
+        buyer_name: '김희찬',
+        buyer_tel: '010-8895-8278',
+      }, rsp => {
+        if (rsp.success) {
+          console.log('결제 성공');
+          const response = await fetchPaymentResult(rsp);
+
+          if (!response.ok) {
+            console.log('결제에 실패했습니다.');
+            return;
+          }
+        } else {
+          console.log('결제 실패');
+          console.log(rsp);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
