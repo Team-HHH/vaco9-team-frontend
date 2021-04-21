9213de0d2144106d478f33c1875596d3fbd0a3dd
hee-c <ways2kim@gmail.com>
2021-04-21 14:12:40 +0900

    Fix apis to include authorization header

--- src/apis/campaigns.js
+++ src/apis/campaigns.js
@@ -1,10 +1,24 @@
-export async function getCampaigns() {
+export async function fetchCampaigns() {
   const url = 'http://localhost:5000/campaign';
   const response = await fetch(url, {
     method: 'GET',
     headers: {
       'Authorization': localStorage.getItem('accessToken'),
     },
+  });
+
+  return response;
+}
+
+export async function fetchNewCampaign(data) {
+  const url = 'http://localhost:5000/campaign';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('accessToken'),
+    },
+    body: JSON.stringify(data),
   });
 
   return response;
