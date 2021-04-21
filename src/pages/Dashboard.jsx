97df7bdf723f21b342d3752825b2a4192e20d5ad
hee-c <ways2kim@gmail.com>
2021-04-21 16:47:58 +0900

    Add styled components to dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { getCampaigns } from '../reducers/campaign';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const NavContainer = styled.div`
+  width: 30%;
+  height: 100%;
+`;
+
+const MainContainer = styled.div`
+width: 70%;
+height: 100%;
+`;
 
 export default function Dashboard() {
   const dispatch = useDispatch();
@@ -10,8 +26,13 @@ export default function Dashboard() {
   }, [dispatch]);
 
   return (
-    <div>
-      dashboard page.
-    </div>
+    <Container>
+      <NavContainer>
+        nav
+      </NavContainer>
+      <MainContainer>
+        main
+      </MainContainer>
+    </Container>
   );
 }
