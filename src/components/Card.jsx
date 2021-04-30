7df142bf855fde9292fdcc741359b941f545ef51
KYOUNGHOYU <qmfahs@naver.com>
2021-04-30 15:17:35 +0900

    Fix CSS of CampaignForm component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,20 +5,22 @@ import PropTypes from 'prop-types';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: ${props => props.width || '400px'};
+  width: ${props => props.width || '90%'};
   height: fit-content;
-  align-items: center;
   justify-content: flex-start;
-  padding: 5px 5px;
+  padding: 15px 15px 15px 0;
+  margin-left: 30px;
 `;
 
-const CardTitle = styled.h3`
+const CardTitle = styled.span`
   margin: 0 0 10px 0;
+  padding-left: 10px;
 `;
 
 const CardItem = styled.div`
   display: flex;
   width: 100%;
+  padding-bottom: 7px;
 `;
 
 export default function Card({ children, title }) {
